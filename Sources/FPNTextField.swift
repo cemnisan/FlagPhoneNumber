2b5c8648f78c74a9b30bbaecb045e9e410d0d75a
Cem <cemnisan@alsay.com.tr>
2025-11-20 10:55:21 +0300

    f

--- Sources/FPNTextField.swift
+++ Sources/FPNTextField.swift
@@ -29,7 +29,7 @@ open class FPNTextField: UITextField {
 
 	/// The size of the leftView
 	private var leftViewSize: CGSize {
-		let width = flagButtonSize.width + getWidth(text: phoneCodeTextField.text!) + 10 + 10 + 5
+        let width = flagButtonSize.width + getWidth(text: phoneCodeTextField.text!) + ((shouldAddLeftBackgroundView == true) ? 35 : 0)
 		let height = bounds.height
 
 		return CGSize(width: width, height: height)
